a5b7b0e6e2063d5b30e6f2fc72cf4f3881b79315
Robério <rroberiosouza@gmail.com>
2024-05-20 19:13:52 -0300

    Manuseando inputs de diferentes tipos

--- lib/entrada-dados.dart
+++ lib/entrada-dados.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class EntradaDados extends StatefulWidget {
   const EntradaDados({super.key});
@@ -8,11 +9,55 @@ class EntradaDados extends StatefulWidget {
 }
 
 class _EntradaDadosState extends State<EntradaDados> {
+  TextEditingController _controllerTexto = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(),
-    );
+        appBar: AppBar(
+          title: Text("Entrada de dados"),
+          backgroundColor: Colors.lightGreen,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            children: [
+              TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(labelText: "Digite um texto"),
+                maxLength: 10,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: "Digite um valor"),
+                obscureText: true,
+                maxLength: 4,
+                maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                onChanged: (String texto) {
+                  print("Texto digitado: " + texto);
+                },
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(labelText: "Digite um e-mail"),
+                style: TextStyle(fontSize: 20, color: Colors.lightGreen),
+                onSubmitted: (String texto) {
+                  print("Texto eviado: " + texto);
+                },
+              ),
+              TextField(
+                keyboardType: TextInputType.datetime,
+                decoration:
+                    InputDecoration(labelText: "Digite uma data ou hora"),
+                controller: _controllerTexto,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print("Texto salvo: " + _controllerTexto.text);
+                  },
+                  child: Text("Salvar"))
+            ],
+          ),
+        ));
   }
 }
