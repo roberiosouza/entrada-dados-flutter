7964936e30a58fae280a4d7b0de3b4732a41d8ba
Robério <rroberiosouza@gmail.com>
2024-05-21 14:03:03 -0300

    Adicionado scroll na tela de entrada-dados

--- lib/entrada-dados.dart
+++ lib/entrada-dados.dart
@@ -18,45 +18,47 @@ class _EntradaDadosState extends State<EntradaDados> {
           title: Text("Entrada de dados"),
           backgroundColor: Colors.lightGreen,
         ),
-        body: Padding(
-          padding: EdgeInsets.all(32),
-          child: Column(
-            children: [
-              TextField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(labelText: "Digite um texto"),
-                maxLength: 10,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(labelText: "Digite um valor"),
-                obscureText: true,
-                maxLength: 4,
-                maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                onChanged: (String texto) {
-                  print("Texto digitado: " + texto);
-                },
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(labelText: "Digite um e-mail"),
-                style: TextStyle(fontSize: 20, color: Colors.lightGreen),
-                onSubmitted: (String texto) {
-                  print("Texto eviado: " + texto);
-                },
-              ),
-              TextField(
-                keyboardType: TextInputType.datetime,
-                decoration:
-                    InputDecoration(labelText: "Digite uma data ou hora"),
-                controller: _controllerTexto,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print("Texto salvo: " + _controllerTexto.text);
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              children: [
+                TextField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(labelText: "Digite um texto"),
+                  maxLength: 10,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(labelText: "Digite um valor"),
+                  obscureText: true,
+                  maxLength: 4,
+                  maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                  onChanged: (String texto) {
+                    print("Texto digitado: " + texto);
                   },
-                  child: Text("Salvar"))
-            ],
+                ),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(labelText: "Digite um e-mail"),
+                  style: TextStyle(fontSize: 20, color: Colors.lightGreen),
+                  onSubmitted: (String texto) {
+                    print("Texto eviado: " + texto);
+                  },
+                ),
+                TextField(
+                  keyboardType: TextInputType.datetime,
+                  decoration:
+                      InputDecoration(labelText: "Digite uma data ou hora"),
+                  controller: _controllerTexto,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      print("Texto salvo: " + _controllerTexto.text);
+                    },
+                    child: Text("Salvar")),
+              ],
+            ),
           ),
         ));
   }
