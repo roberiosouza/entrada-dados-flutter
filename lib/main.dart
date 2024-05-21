fb531a14dbe3270aca2dfaa901838464b6f6d8c9
Robério <rroberiosouza@gmail.com>
2024-05-21 14:03:38 -0300

    Adicionado uma nova home de entrada de dados com checkbox.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:entrada_dados/entrada-dados.dart';
+import 'package:entrada_dados/entrada-checkbox.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: EntradaDados(),
+    // home: EntradaDados(),
+    home: EntradaCheckbox(),
   ));
 }
